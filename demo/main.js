e17f9105b698903159dae9d1bc9908367d24f001
OliverXH <1556423608@qq.com>
2020-07-22 14:14:25 +0800

    Update main.js

--- demo/main.js
+++ demo/main.js
@@ -11,17 +11,44 @@ let scene = engine.scene;
 let world = engine.world;
 // world.allowSleep = false;
 
-let startBtn = document.getElementsByTagName("button")[0];
+let gui = engine.gui;
+
+//====================================================================================
+//  Test 
+//====================================================================================
+let options = {
+    Type: 'sphere',
+    Start_and_Reset: function () {
+        console.log(options.Type);
+        start(options.Type);
+    }
+};
+
+gui.add(options, 'Start_and_Reset');
 
 let boxes = [];
+boxes.length = 0;
 
-startBtn.onclick = function () {
+gui.add(options, 'Type', { Sphere: 'sphere', Box: 'box' }).onChange((type) => {
+
+    if (boxes.length > 0) {
+        changed(type);
+    }
+
+    console.log(type);
+});
+
+function start(_type) {
+
+    console.log('start');
+
     if (boxes.length > 0) {
         // console.log(initPosition);
 
         for (let i = 0; i < boxes.length; i++) {
 
             boxes[i].body.position.copy(boxes[i].initPosition);
+            // boxes[i].body.position.set(boxes[i].initPosition.x, boxes[i].initPosition.y, boxes[i].initPosition.z);
 
             boxes[i].body.velocity.set(0, 0, 0);
             boxes[i].body.angularVelocity.set(0, 0, 0);
@@ -29,36 +56,101 @@ startBtn.onclick = function () {
 
         }
     } else {
+        console.log('create', _type);
 
         let boxes_position = new THREE.Vector3(0, 0, 20);
 
-        for (let i = 0; i < 1; i++) {
-            for (let j = 0; j < 10; j++) {
-                for (let k = 0; k < 10; k++) {
-                    let box;
+        switch (_type) {
+            case 'sphere':
+                console.log('0');
 
-                    box = new THREE.Mesh(
-                        // new THREE.BoxGeometry(1, 1, 1),
-                        new THREE.SphereGeometry(0.5, 30, 30),
-                        new THREE.MeshLambertMaterial({ color: 0xff0000 })
-                    );
-                    scene.add(box);
+                for (let i = 0; i < 1; i++) {
+                    for (let j = 0; j < 10; j++) {
+                        for (let k = 0; k < 10; k++) {
+                            let sphere = createSphere(0.5, 1);
 
-                    box.body = new CANNON.Body({
-                        mass: 4,
-                        // shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5))
-                        shape: new CANNON.Sphere(0.5)
-                    });
-                    box.initPosition = new CANNON.Vec3(boxes_position.x + 2 * k - 9.5, boxes_position.y + 2 * j - 9.5, boxes_position.z + 2 * i - 0.5);
+                            scene.add(sphere);
+                            world.addBody(sphere.body);
 
-                    box.body.position.copy(box.initPosition);
+                            sphere.initPosition = new CANNON.Vec3(boxes_position.x + 2 * k - 9.5, boxes_position.y + 2 * j - 9.5, boxes_position.z + 2 * i - 0.5);
 
-                    world.addBody(box.body);
+                            sphere.body.position.copy(sphere.initPosition);
 
-                    boxes.push(box);
+                            boxes.push(sphere);
+                        }
+                    }
                 }
-            }
+                break;
+
+            case 'box':
+                console.log('1');
+
+                for (let i = 0; i < 1; i++) {
+                    for (let j = 0; j < 10; j++) {
+                        for (let k = 0; k < 10; k++) {
+                            let box = createBox(1, 1);
+
+                            scene.add(box);
+                            world.addBody(box.body);
+
+                            box.initPosition = new CANNON.Vec3(boxes_position.x + 2 * k - 9.5, boxes_position.y + 2 * j - 9.5, boxes_position.z + 2 * i - 0.5);
+
+                            box.body.position.copy(box.initPosition);
+
+                            boxes.push(box);
+                        }
+                    }
+                }
+                break;
         }
+
+        function createSphere(_radius, _mass) {
+            let mesh = new THREE.Mesh(
+                new THREE.SphereGeometry(_radius, 30, 30),
+                new THREE.MeshLambertMaterial({ color: 0xff0000 })
+            );
+
+            mesh.body = new CANNON.Body({
+                mass: _mass,
+                shape: new CANNON.Sphere(_radius)
+            });
+
+            return mesh;
+        }
+
+        function createBox(_edgeLength, _mass) {
+            let mesh = new THREE.Mesh(
+                new THREE.BoxGeometry(_edgeLength, _edgeLength, _edgeLength),
+                new THREE.MeshLambertMaterial({ color: 0xff0000 })
+            );
+
+            mesh.body = new CANNON.Body({
+                mass: _mass,
+                shape: new CANNON.Box(new CANNON.Vec3(0.5 * _edgeLength, 0.5 * _edgeLength, 0.5 * _edgeLength))
+            });
+
+            return mesh;
+        }
+    }
+}
+
+function changed(_type) {
+    switch (_type) {
+        case 'sphere':
+            boxes.forEach((sphere) => {
+                sphere.geometry = new THREE.SphereGeometry(0.5, 30, 30)
+                sphere.body.shapes.length = 0;
+                sphere.body.addShape(new CANNON.Sphere(0.5));
+            });
+            break;
+
+        case 'box':
+            boxes.forEach((box) => {
+                box.geometry = new THREE.BoxGeometry(1, 1, 1);
+                box.body.shapes.length = 0;
+                box.body.addShape(new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)));
+            });
+            break;
     }
 }
 //======================================================================
