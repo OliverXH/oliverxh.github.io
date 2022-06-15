060c58baf692757c26f5b25befa8c0bfc22f5973
OliverXH <1556423608@qq.com>
2022-06-15 19:48:28 +0800

    Update app.module.js

--- demo/js/app.module.js
+++ demo/js/app.module.js
@@ -17488,13 +17488,16 @@ class Application extends EventEmitter {
         this.renderer.setPixelRatio( window.devicePixelRatio );
         this.renderer.setClearColor( options.clearColor ?? 0x000000, 1 );
 
-        this.renderer.physicallyCorrectLights = options.physicallyCorrect ?? true;
+        this.renderer.physicallyCorrectLights = true;
         this.renderer.gammaOutPut = true;
-        this.renderer.outputEncoding = options.outputEncoding ?? sRGBEncoding;
+        this.renderer.outputEncoding = sRGBEncoding;
         this.renderer.shadowMap.enabled = options.shadow ?? true;
         this.renderer.shadowMap.type = PCFSoftShadowMap;
         if ( options.toneMapping ) {
             switch ( options.toneMapping ) {
+                // case 'None':
+                //     this.renderer.toneMapping = NoToneMapping;
+                //     break;
                 case 'Linear':
                     this.renderer.toneMapping = LinearToneMapping;
                     break;
@@ -17531,7 +17534,7 @@ class Application extends EventEmitter {
         this.camera = new PerspectiveCamera();
         this.camera.fov = 55;
         // this.camera.position.set(13, 16, 14);
-        this.camera.position.set( 1, 2, 5 );
+        this.camera.position.set( 4, 2, 4 );
         this.camera.lookAt( this.root.position );
 
         this.root.add( this.camera );
@@ -17883,48 +17886,45 @@ class Application extends EventEmitter {
 
     // ==========================================================================
 
-    // check for wasm module support
-    wasmSupported() {
-        try {
-            if ( typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function" ) {
-                const module = new WebAssembly.Module( Uint8Array.of( 0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00 ) );
-                if ( module instanceof WebAssembly.Module )
-                    return new WebAssembly.Instance( module ) instanceof WebAssembly.Instance;
-            }
-        } catch ( e ) { }
-        return false;
-    }
-
-    // load a script
-    loadScript( url, async = true, doneCallback = () => { } ) {
-        let tag = document.createElement( 'script' );
-        tag.onload = function () {
-            doneCallback();
-        };
-        tag.onerror = function () {
-            throw new Error( 'failed to load ' + url );
-        };
-        tag.async = async;
-        tag.src = url;
-        document.head.appendChild( tag );
-    }
-
-    // load and initialize a wasm module
-    loadWasmModuleAsync( moduleName, jsUrl, binaryUrl, doneCallback ) {
-        scope.loadScript( jsUrl, true, function () {
-            let lib = window[ moduleName ];
-            window[ moduleName + 'Lib' ] = lib;
-            lib( { locateFile: function () { return binaryUrl; } } ).then( function ( instance ) {
-                window[ moduleName ] = instance;
-                doneCallback();
-            } );
-        } );
-    }
-
-
     loadModules( modules, urlPrefix, doneCallback ) {
 
-        let scope = this;
+        // check for wasm module support
+        function wasmSupported() {
+            try {
+                if ( typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function" ) {
+                    const module = new WebAssembly.Module( Uint8Array.of( 0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00 ) );
+                    if ( module instanceof WebAssembly.Module )
+                        return new WebAssembly.Instance( module ) instanceof WebAssembly.Instance;
+                }
+            } catch ( e ) { }
+            return false;
+        }
+
+        // load a script
+        function loadScriptAsync( url, doneCallback ) {
+            let tag = document.createElement( 'script' );
+            tag.onload = function () {
+                doneCallback();
+            };
+            tag.onerror = function () {
+                throw new Error( 'failed to load ' + url );
+            };
+            tag.async = true;
+            tag.src = url;
+            document.head.appendChild( tag );
+        }
+
+        // load and initialize a wasm module
+        function loadWasmModuleAsync( moduleName, jsUrl, binaryUrl, doneCallback ) {
+            loadScriptAsync( jsUrl, function () {
+                let lib = window[ moduleName ];
+                window[ moduleName + 'Lib' ] = lib;
+                lib( { locateFile: function () { return binaryUrl; } } ).then( function ( instance ) {
+                    window[ moduleName ] = instance;
+                    doneCallback();
+                } );
+            } );
+        }
 
         if ( typeof modules === "undefined" || modules.length === 0 ) {
             // caller may depend on callback behaviour being async
@@ -17938,16 +17938,16 @@ class Application extends EventEmitter {
                 }
             };
 
-            let wasm = scope.wasmSupported();
+            let wasm = wasmSupported();
             modules.forEach( function ( m ) {
                 if ( !m.hasOwnProperty( 'preload' ) || m.preload ) {
                     if ( wasm && m.glueUrl ) {
-                        scope.loadWasmModuleAsync( m.moduleName, urlPrefix + m.glueUrl, urlPrefix + m.wasmUrl, asyncCallback );
+                        loadWasmModuleAsync( m.moduleName, urlPrefix + m.glueUrl, urlPrefix + m.wasmUrl, asyncCallback );
                     } else {
                         if ( !m.fallbackUrl ) {
                             throw new Error( 'wasm not supported and no fallback supplied for module ' + m.moduleName );
                         }
-                        scope.loadWasmModuleAsync( m.moduleName, urlPrefix + m.fallbackUrl, "", asyncCallback );
+                        loadWasmModuleAsync( m.moduleName, urlPrefix + m.fallbackUrl, "", asyncCallback );
                     }
                 } else {
                     asyncCallback();
